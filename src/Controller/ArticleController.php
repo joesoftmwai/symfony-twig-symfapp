680f94fe9e260735b609c59f871aae3a9c8f63f3
Mwai Joseph <joesoftmwai@gmail.com>
2021-09-14 14:28:15 +0300

    Utilized doctrine to setup DB & related tables, also defined some Entities

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
@@ -21,5 +22,14 @@ class ArticleController extends AbstractController
             "Article 1", "Article 2"
         ];
         return $this->render('articles/index.html.twig', array('articles' => $articles));
+    }
+
+    /**
+     * @Route("/article/save")
+     */
+    public function save()
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $article = new Article();
     }
 }
